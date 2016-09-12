d879773ee54fc75d2a405f2fa7e7b35fa32ff7a3
Wessel <w.j.becker@st.hanze.nl>
2016-09-12 14:50:43 +0200

    ADD: the final stuff

--- src/main/java/everything.java
+++ src/main/java/everything.java
@@ -4,6 +4,7 @@ import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static java.nio.file.FileSystems.getDefault;
 import static java.nio.file.Files.lines;
@@ -16,28 +17,44 @@ public class everything {
     public static final String ALPHABET = "abcdefghijklmnopqrstufwxyz";
 
     public static void main(String[] args) throws IOException {
-        Path path = getDefault().getPath("./encrypthed");
+        Path path = getDefault().getPath("./encrypted");
         char[] encrypted = lines(path).map(String::toCharArray)
                 .reduce(ArrayUtils::addAll)
                 .get();
 
         StandardDeviation standardDeviation = new StandardDeviation();
-        double[] stdDevSums = new double[10];
+        double[] stdDevSums = new double[11];
         for (int i = 5; i <= 15; ++i) {
+            System.out.printf("==========================================\nFor keySize %d \n", i);
             for (int k = 0; k <= i; ++k) {
                 ArrayList<Character> charList = new ArrayList<>();
                 for (int j = i; j < encrypted.length; j += i) {
                     charList.add(encrypted[j]);
                 }
-
-                stdDevSums[i] += standardDeviation.evaluate(getFrequencies(charList));
+                double[] frequencies = getFrequencies(charList);
+                stdDevSums[i - 5] += standardDeviation.evaluate(frequencies);
+                printFrequencies(frequencies, k);
             }
         }
 
+        printStdDevs(stdDevSums);
+    }
+
+    private static void printStdDevs(double[] stdDevSums) {
+        System.out.println("==========================================");
         int i = 5;
         for (double stdDevSum : stdDevSums) {
-            System.out.printf("Sum of  %d std. devs: %f\n", i, stdDevSum);
+            System.out.printf("Sum of  %d std. devs: %f\n", i++, stdDevSum);
         }
+        System.out.println("==========================================");
+    }
+
+    private static void printFrequencies(double[] frequencies, int charCount) {
+        String concattenated = Arrays.stream(frequencies)
+                .mapToObj(Double::toString)
+                .reduce(((left, right) -> left + ", " + right))
+                .get();
+        System.out.printf("For %d th letter: {%s }\n", charCount, concattenated);
     }
 
     private static double[] getFrequencies(ArrayList<Character> charList) {
